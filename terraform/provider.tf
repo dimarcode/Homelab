0d0e6daf96d886f96a017f7ce7b9857e288b0172
Cody <codydimarcolr@gmail.com>
2026-01-26 03:54:58 +0000

    added some terraform files

---
+++ terraform/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.93.0" # x-release-please-version
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint  = var.virtual_environment_endpoint
+  api_token = var.virtual_environment_token
+}
